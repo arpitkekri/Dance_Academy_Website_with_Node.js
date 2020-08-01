21250308bee0a90dbe04d4df5b0c95ceb49310f6
arpitkekri <jainarpitkekri@gmail.com>
2020-08-02 03:21:19 +0530

    Designing frontend using pug

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ app.use(express.urlencoded())
 // PUG SPECIFIC STUFF
 app.set('view engine', 'pug') // Set the template engine as pug
 app.set('views', path.join(__dirname, 'views')) // Set the views directory
+ 
+// ENDPOINTS
+app.get('/', (req, res)=>{ 
+    const params = { }
+    res.status(200).render('index.pug', params);
+})
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
