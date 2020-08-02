0e1374192420317aadb15ae611b949f06bacbc24
arpitkekri <jainarpitkekri@gmail.com>
2020-08-02 16:09:29 +0530

    contact us form with template inheritence

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ app.set('views', path.join(__dirname, 'views')) // Set the views directory
 // ENDPOINTS
 app.get('/', (req, res)=>{ 
     const params = { }
-    res.status(200).render('index.pug', params);
+    res.status(200).render('home.pug', params);
+})
+
+app.get('/contact', (req, res)=>{ 
+    const params = { }
+    res.status(200).render('contact.pug', params);
 })
 
 // START THE SERVER
